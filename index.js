ed2da9add2a390591ff31b5fd70115360967da2a
1bsilver <ibrahim.silver20@gmail.com>
2021-03-06 22:59:12 +0530

    step 5

--- index.js
+++ index.js
@@ -61,6 +61,32 @@ app.delete('/api/persons/:id', (request,response) => {
     response.status(204).end()
 })
 
+const generateId = () => {
+    const maxID = persons.length > 0 
+    ? Math.max(...persons.map(n=>n.id))
+    : 0
+    return maxID +1
+}
+
+app.post('/api/persons/', (request, response) => {
+    const body = request.body
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT , ()=> {
     console.log(`Server running on port ${PORT}`)
