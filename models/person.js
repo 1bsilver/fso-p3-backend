ff2bbb1c0377c3437e90418adebffa691f5faff5
Fang <ibrahim.silver20@gmail.com>
2021-07-28 23:24:05 +0530

    added unique validator to the backend

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URL
 
@@ -13,9 +14,11 @@ mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindA
 })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {type: String, required: true, unique:true},
+    number: {type: String, require: true}
 })
+
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
